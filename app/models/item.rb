7ea39b949e33673ccaf81645c1b82afedee78015
nennekotaro <72799169+nennekotaro@users.noreply.github.com>
2023-04-18 16:03:19 +0900

    itemのモデル単体テストコード

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,19 +4,20 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
+ 
   belongs_to :category
   belongs_to :sales_status
   belongs_to :shipping_fee_status
   belongs_to :prefecture
   belongs_to :scheduled_delivery
 
-  validates :image, presence: true, content_type: :image, message: "can't be blank"
-  validates :name, presence: true
-  validates :info, presence: true
-  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999},format: { with: /\A\d+\z/}, message: "is invalid. Input half-width characters"
-  validates :category_id, numericality: { other_than: 1, message: "can't be blank"}
-  validates :sales_status_id, numericality: { other_than: 1, message: "can't be blank"}
-  validates :shipping_fee_status_id, numericality: { other_than: 1, message: "can't be blank"}
-  validates :prefecture_id , numericality: { other_than: 1, message: "can't be blank"}
-  validates :scheduled_delivery_id , numericality: { other_than: 1, message: "can't be blank"}
+  validates :image, presence: true
+  validates :name, presence: true, length: { maximum: 40 }
+  validates :info, presence: true, length: { maximum: 1000 }
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is invalid. Input half-width characters"},format: { with: /\A\d+\z/, message: "is out of setting range"}
+  validates :category_id, presence: true
+  validates :sales_status_id, presence: true
+  validates :shipping_fee_status_id, presence: true
+  validates :prefecture_id , presence: true
+  validates :scheduled_delivery_id , presence: true
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,16 @@
 FactoryBot.define do
   factory :item do
-    
+    name                { Faker::Lorem.word }
+    info                { Faker::Lorem.sentence }
+    price               { Faker::Number.within(range: 300..9999999) }
+    category            { Category.all.sample } 
+    sales_status        { SalesStatus.all.sample }
+    prefecture          { Prefecture.all.sample }
+    scheduled_delivery  { ScheduledDelivery.all.sample }
+    shipping_fee_status { ShippingFeeStatus.all.sample } 
+    association :user
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
