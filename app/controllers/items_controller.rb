d4a38d3c2197dfe608464a7fbe28a25b4e2df850
nennekotaro <72799169+nennekotaro@users.noreply.github.com>
2023-04-19 14:54:14 +0900

    コードレビュー後修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :create]
-  before_action :move_to_index, only: [:edit]
   before_action :set_item, only: [:show, :edit, :update]
+  before_action :move_to_index, only: [:edit]
+
 
   def index
     @items = Item.includes(:user).order(created_at: :desc)
@@ -46,9 +47,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    item = Item.find(params[:id])
-    return if current_user.id == item.user.id
-
-    redirect_to root_path
+    return if current_user.id == @item.user.id
+      redirect_to root_path
   end
 end
