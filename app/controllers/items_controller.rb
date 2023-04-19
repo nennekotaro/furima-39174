8a510b36e3d1562c8ce4261be22bc0442e5c8828
nennekotaro <72799169+nennekotaro@users.noreply.github.com>
2023-04-19 15:13:16 +0900

    bundle exec rubocop

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,6 @@ class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :move_to_index, only: [:edit]
 
-
   def index
     @items = Item.includes(:user).order(created_at: :desc)
   end
@@ -53,6 +52,7 @@ class ItemsController < ApplicationController
 
   def move_to_index
     return if current_user.id == @item.user.id
-      redirect_to root_path
+
+    redirect_to root_path
   end
 end
